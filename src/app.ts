944e0dc22eaf8262613981bf3a5a7943fb9aa111
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2024-08-18 10:27:02 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -382,15 +382,15 @@ app.get('/get_action', async (req, res) => {
         actions: [
           {
             "label": choiceOne,
-            "href": `http://localhost:8000/post_action?choice=${encodeURIComponent(choiceOne)}`
+            "href": `https://gamebook-m532.onrender.com/post_action?choice=${encodeURIComponent(choiceOne)}`
           },
           {
             "label": choiceTwo,
-            "href": `http://localhost:8000/post_action?choice=${encodeURIComponent(choiceTwo)}`
+            "href": `https://gamebook-m532.onrender.com/post_action?choice=${encodeURIComponent(choiceTwo)}`
           },
           {
             "label": choiceThree,
-            "href": `http://localhost:8000/post_action?choice=${encodeURIComponent(choiceThree)}`
+            "href": `https://gamebook-m532.onrender.com/post_action?choice=${encodeURIComponent(choiceThree)}`
           }
         ]
       }
